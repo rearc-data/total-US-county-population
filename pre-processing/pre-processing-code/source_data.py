90091aff8c2172577487fc54e08ee4ab4ad25f15
Ruchika Sah <ruchika817@users.noreply.github.com>
2021-05-07 11:10:28 -0400

    Update source_data.py

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -9,7 +9,7 @@ from boto3.s3.transfer import TransferConfig
 from io import BytesIO
 
 def source_dataset():
-    source_dataset_url = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=PERMIT"
+    # source_dataset_url = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=PERMIT"
     
     response = None
     retries = 5
